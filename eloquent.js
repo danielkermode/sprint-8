80809d247ed94e21cca6bfd36fe01a23847ac699
danielkermode <danielkermode@hotmail.co.nz>
2016-05-09 18:15:56 +1200

    other algs

--- eloquent.js
+++ eloquent.js
@@ -1,4 +1,4 @@
-//Sum of a Range
+/***Sum of a Range***/
 function range(first, last, step) {
   var stepper = typeof step === 'number'? step : 1;
   var dir = Math.sign(last - first);
@@ -29,13 +29,13 @@ function sum(arr) {
   return arr.reduce(function(a, b) {return a + b;})
 }
 
-/*
+/***
 Reversing an Array
 functions and side effects: the reverseArray is a more functional style since it has no side effects,
 so is easier to reason about.
 However, reverseArrayInPlace is more efficient since it only has to check half the array's length
 (rounded down) and doesn't have to create a new array in the process.
-*/
+***/
 function reverseArray(arr) {
   if(!(arr instanceof Array)) throw new Error('input must be an array');
   var newArr = arr.slice();
@@ -54,4 +54,90 @@ function reverseArrayInPlace(arr) {
     arr[(length - 1) - i] = current;
   }
   return arr;
+}
+
+/***List***/
+function LinkedList(val) {
+  this.head = val || null;
+}
+
+LinkedList.prototype.push = function(val) {
+  var node = {
+    value: val,
+    rest: null
+  };
+  if(!this.head) {
+    this.head = node;
+  } else {
+    current = this.head;
+    while(current.rest) {
+      current = current.rest;
+    }
+    current.rest = node;
+  }
+}
+
+LinkedList.prototype.toArray = function() {
+  var arr = [];
+  if(!this.head.rest) {
+    arr.push(this.head.value);
+    return arr;
+  } else {
+    current = this.head;
+    arr.push(this.head.value);
+    while(current.rest) {
+      current = current.rest;
+      arr.push(current.value);
+    }
+    return arr;
+  }
+}
+
+LinkedList.prototype.prepend = function(val) {
+  var node = {
+    value: val,
+    rest: this.head
+  };
+  this.head = node;
+}
+
+function arrayToList(arr) {
+  if(!(arr instanceof Array)) throw new Error('input must be an array');
+  var list = new LinkedList();
+  arr.forEach(function(val) {
+    list.push(val);
+  });
+  return list.head;
+}
+
+function listToArray(list) {
+  newList = list.head? list : new LinkedList(list);
+  return newList.toArray();
+}
+
+function prepend(val, list) {
+  newList = !(list instanceof LinkedList)? new LinkedList(list) : list;
+  newList.prepend(val);
+  return newList.head;
+}
+
+function nth(list, ind) {
+  newList = !(list instanceof LinkedList)? new LinkedList(list) : list;
+  return newList.toArray()[ind];
+}
+
+function nthRecursive(list, ind) {
+  return ind === 0? list.value : nthRecursive(list.rest, ind - 1)
+}
+/***Deep Comparison***/
+function deepEqual(obj1, obj2) {
+  var res = true;
+  for(var key in obj1) {
+    if(typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      res = deepEqual(obj1[key], obj2[key]);
+    } else {
+      if(obj1[key] !== obj2[key]) res = false;
+    }
+  }
+  return res;
 }
